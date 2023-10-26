2feb8e60bac92c8cde8a3887bb8eae6abf98692d
Div <devilliers.basson@gmail.com>
2023-10-26 23:06:47 +0200

    initial commit of script.

---
+++ setup.sh
@@ -0,0 +1,105 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install build-essential -y
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Configure Homebrew
+echo >> ~/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Brew doctor
+brew doctor
+
+# Install GCC
+brew install gcc
+
+# Install Cockpit
+sudo apt install -t ${VERSION_CODENAME}-backports cockpit -y
+
+# Setup 45drives repo
+curl -sSL https://repo.45drives.com/setup | sudo bash
+
+# Install Cockpit packages
+sudo apt install cockpit-file-sharing cockpit-identities -y
+
+# Install dependencies
+sudo apt install ffmpeg -y
+
+# Install NVIDIA GPU drivers
+if lspci | grep -i nvidia; then
+  echo "Nvidia GPU detected"
+  sudo apt update && sudo apt install nvidia-driver-515 nvidia-dkms-515 nvidia-smi -y
+else
+  echo "Nvidia GPU not detected"
+fi
+
+# Install AMD GPU drivers
+if lspci | grep -i amd; then
+  echo "AMD GPU detected"
+  sudo apt update
+  wget https://repo.radeon.com/amdgpu-install/22.40.5/ubuntu/jammy/amdgpu-install_5.4.50405-1_all.deb
+  sudo chmod +x amdgpu-install_5.4.50405-1_all.deb
+  sudo apt install ./amdgpu-install_5.4.50405-1_all.deb -y
+  sudo amdgpu-install --usecase=rocm -y --accept-eula --no-32
+  sudo apt install vainfo radeontop docker-compose libgl1-mesa-glx libgl1-mesa-dri xserver-xorg-video-amdgpu -y
+  sudo usermod -a -G video $LOGNAME
+  sudo usermod -a -G render $LOGNAME
+else
+  echo "AMD GPU not detected"
+fi
+
+# Install Coral EdgeTpu packages
+if lspci -nn | grep 089a; then
+  echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  sudo apt update
+  sudo apt install gasket-dkms libedgetpu1-std
+  sudo sh -c "echo 'SUBSYSTEM==\"apex\", MODE=\"0660\", GROUP=\"apex\"' >> /etc/udev/rules.d/65-apex.rules"
+  sudo groupadd apex
+  sudo adduser $USER apex
+else
+  echo "Coral EdgeTpu PCIE not detected"
+fi
+
+# Update and upgrade system
+sudo apt update && sudo apt upgrade -y
+
+# Install NFS packages
+sudo apt install nfs-utils nfs-common -y
+
+# Install CasaOS
+curl -fsSL https://get.casaos.io | sudo bash
+
+# Register app stores in CasaOS
+casaos-cli app-management register app-store https://github.com/IceWhaleTech/_appstore/archive/refs/heads/main.zip
+sleep 4
+casaos-cli app-management register app-store https://casaos-appstore.paodayag.dev/linuxserver.zip
+sleep 4
+casaos-cli app-management register app-store https://play.cuse.eu.org/Cp0204-AppStore-Play.zip
+sleep 4
+casaos-cli app-management register app-store https://casaos-appstore.paodayag.dev/coolstore.zip
+sleep 4
+casaos-cli app-management register app-store https://paodayag.dev/casaos-appstore-edge.zip
+sleep 4
+casaos-cli app-management register app-store https://github.com/mr-manuel/CasaOS-HomeAutomation-AppStore/archive/refs/tags/latest.zip
+sleep 4
+casaos-cli app-management register app-store https://github.com/bigbeartechworld/big-bear-casaos/archive/refs/heads/master.zip
+sleep 4
+casaos-cli app-management register app-store https://github.com/mariosemes/CasaOS-TMCstore/archive/refs/heads/main.zip
+sleep 4
+casaos-cli app-management register app-store https://github.com/mariosemes/CasaOS-TMCstore/archive/refs/heads/main.zip
+sleep 4
+casaos-cli app-management register app-store https://github.com/arch3rPro/Pentest-Docker/archive/refs/heads/master.zip
+sleep 4
+casaos-cli app-management register app-store https://github.com/tigerinus/yet-another-casaos-appstore/archive/refs/heads/main.zip
+sleep 4
+casaos-cli app-management register app-store https://github.com/cloudrack-ca/Cloudrack-CasaOS-App-Repo/archive/refs/heads/master.zip
+sleep 4
+casaos-cli app-management register app-store http://104.234.11.251/CasaOS-Custom-AppStore.zip
+sleep 4
+casaos-cli app-management register app-store https://github.com/Double-A-92/CasaOS-AppStore/archive/refs/heads/main.zip
+
+sudo systemctl restart casaos-app-management.service
