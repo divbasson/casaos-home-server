38719b1b488b5da214eeabffeb16183a06aee237
Div <devilliers.basson@gmail.com>
2023-10-28 14:45:39 +0200

    ip find_and_replace_ip

--- docker-compose/find_and_replace_ip.sh
+++ docker-compose/find_and_replace_ip.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Function to replace IP addresses in a file
-replace_ip_in_file() {
+# Function to search for IP addresses in a file and print them
+search_ip_in_file() {
     local file="$1"
-    local old_ips=("192.168.0." "192.168.1.")
-    local new_ip="$2"
-
-    for ip in "${old_ips[@]}"; do
-        # Use sed to replace IP addresses in the file
-        sed -i "s/$ip[0-9]\{1,3\}/$new_ip/g" "$file"
-    done
+    
+    # Use grep to search for the pattern in the file and print the matching lines
+    grep -oE '192\.168\.0\.[0-9]{1,3}' "$file"
 }
 
-# Prompt the user for the new IP address
-read -p "Enter the new IP address: " new_ip
-
 # Find all files in the current directory and its subdirectories
-find . -type f -exec sh -c 'replace_ip_in_file "$0" "$1"' {} "$new_ip" \;
-
-echo "IP addresses in files have been replaced with $new_ip."
+find . -type f -print0 | while IFS= read -r -d $'\0' file; do
+    echo "Searching in $file:"
+    search_ip_in_file "$file"
+    echo "--------------------------------------"
+done
